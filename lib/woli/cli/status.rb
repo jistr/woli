a3c38be62c83fa6d0cd2a08610bf3f2db9a921f5
Jiří Stránský <jistr@jistr.com>
2012-07-13 21:40:22 +0200

    shorten local var all_entries_dates to entries_dates

--- lib/woli/cli/status.rb
+++ lib/woli/cli/status.rb
@@ -2,14 +2,14 @@ module Woli
   class Cli
     desc 'status', 'Print basic information about the diary.'
     def status
-      all_entries_dates = Woli.diary.all_entries_dates
+      entries_dates = Woli.diary.all_entries_dates
       date_format = '%d-%m-%Y'
-      coverage = all_entries_dates.count / Float(all_entries_dates.last - all_entries_dates.first)
+      coverage = entries_dates.count / Float(entries_dates.last - entries_dates.first)
 
       puts "Woli Diary Status"
       puts "================="
-      puts "First entry: #{all_entries_dates.first.strftime(date_format)}"
-      puts "Last entry: #{all_entries_dates.last.strftime(date_format)}"
+      puts "First entry: #{entries_dates.first.strftime(date_format)}"
+      puts "Last entry: #{entries_dates.last.strftime(date_format)}"
       puts "Coverage: %2d %" % (coverage * 100).round
       puts "Missing entries since the last one: #{Woli.diary.missing_entries_count}"
     end
