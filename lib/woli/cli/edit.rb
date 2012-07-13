8d4d60a0df584048c3781621d53ff55c15a438ea
Jiří Stránský <jistr@jistr.com>
2012-07-13 20:30:16 +0200

    kinda smart day parsing for edit command

--- lib/woli/cli/edit.rb
+++ lib/woli/cli/edit.rb
@@ -8,14 +8,18 @@ module Woli
 
       1) Implicitly: If no date is given, 'today' is assumed.
 
-      2) Via keywords: woli edit [today|yesterday]
+      2) Via a keyword: woli edit DAYNAME
+      (Where DAYNAME can be:
+      'today' or 't';
+      'yesterday' or 'y')
 
-      3) Via date: woli edit DD[-MM[-YYYY]]
-      Use hyphens, dots or slashes to separate day/month/year.
-      If year or month is not given, current year (current month) is assumed.
+      3) Via a date: woli edit DD[-MM[-YYYY]]
+      (Use hyphens, dots or slashes to separate day/month/year.
+      If year or month is not given, current year / current month is assumed.)
+
+      4) Via days ago: woli edit ^DAYS
+      (Where DAYS is a number of days ago. ^1 is yesterday, ^7 is a week ago etc.)
     END
-      # 4) Via days ago: woli edit ^DAYS
-      # Where DAYS is a number of days ago (^1 is yesterday, ^7 is a week ago etc.)
     def edit(fuzzy_date = 'today')
       date = parse_date(fuzzy_date)
       entry = Woli.repository.load_entry(date) || DiaryEntry.new(date, '', Woli.repository)
@@ -31,8 +35,24 @@ module Woli
     private
 
     def parse_date(fuzzy_date)
-      # FIXME really perform parsing
-      return DateTime.now
+      case fuzzy_date
+      when 'today', 't', nil
+        Date.today
+      when 'yesterday', 'y'
+        Date.today - 1
+      when /\A\^(?<days_ago>\d+)/
+        Date.today - Integer($~[:days_ago])
+      when /\A(?<day>\d+)([-\.\/](?<month>\d+)([-\.\/](?<year>\d+))?)?\Z/
+        today = Date.today
+        year = $~[:year] ? Integer($~[:year]) : today.year
+        month = $~[:month] ? Integer($~[:month]) : today.month
+        day = $~[:day] ? Integer($~[:day]) : today.day
+        Date.new(year, month, day)
+      else
+        self.class.task_help(shell, 'edit')
+        puts
+        raise MalformattedArgumentError, "'#{fuzzy_date}' is not a valid way to specify a date."
+      end
     end
 
     def save_text_to_temp_file(entry, temp_file_name)
