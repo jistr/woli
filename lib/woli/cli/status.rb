be76a6bd1ebf38986d55ec6113854d2767f557ec
Jiří Stránský <jistr@jistr.com>
2012-07-16 21:06:25 +0200

    status task informs of empty diary

--- lib/woli/cli/status.rb
+++ lib/woli/cli/status.rb
@@ -4,14 +4,19 @@ module Woli
     def status
       entries_dates = Woli.diary.all_entries_dates
       date_format = '%d-%m-%Y'
-      coverage = entries_dates.count / Float(entries_dates.last - entries_dates.first + 1)
 
       puts "Woli Diary Status"
       puts "================="
-      puts "First entry: #{entries_dates.first.strftime(date_format)}"
-      puts "Last entry: #{entries_dates.last.strftime(date_format)}"
-      puts "Coverage: %2d %" % (coverage * 100).round
-      puts "Missing entries since the last one: #{Woli.diary.missing_entries_count}"
+
+      if entries_dates.count > 0
+        coverage = entries_dates.count / Float(entries_dates.last - entries_dates.first + 1)
+        puts "First entry: #{entries_dates.first.strftime(date_format)}"
+        puts "Last entry: #{entries_dates.last.strftime(date_format)}"
+        puts "Coverage: %2d %" % (coverage * 100).round
+        puts "Missing entries since the last one: #{Woli.diary.missing_entries_count}"
+      else
+        puts "Diary is empty. Run 'woli help edit' to learn how to create entries."
+      end
     rescue ConfigError => e
       raise Thor::Error, e.message
     end
