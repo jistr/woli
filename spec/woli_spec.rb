cedad9983cd3d2c619410e56bdcfe02d7b00d4fd
Jiří Stránský <jistr@jistr.com>
2012-07-16 22:21:18 +0200

    Woli#diary spec

--- spec/woli_spec.rb
+++ spec/woli_spec.rb
@@ -21,4 +21,31 @@ describe Woli do
       Woli.config.must_equal :fake_config
     end
   end
+
+  describe ".diary" do
+    before do
+      module Woli
+        @repository = :fake_repository
+      end
+    end
+
+    after do
+      module Woli
+        @diary = nil
+        @repository = nil
+      end
+    end
+
+    it "returns a diary when already created" do
+      module Woli
+        @diary = :fake_diary
+      end
+      Woli.diary.must_equal :fake_diary
+    end
+
+    it "creates a diary if not created yet" do
+      Woli::Diary.expects(:new).with(:fake_repository).returns(:fake_diary)
+      Woli.diary.must_equal :fake_diary
+    end
+  end
 end
