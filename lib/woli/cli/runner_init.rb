821cc86ef064e57271c1be0bb2f4baf67bac58bb
Jiří Stránský <jistr@jistr.com>
2012-07-17 22:42:04 +0200

    init task uses #say instead of puts, specs for it

--- lib/woli/cli/runner_init.rb
+++ lib/woli/cli/runner_init.rb
@@ -8,9 +8,9 @@ module Woli
       def init
         created_config_file_path = Woli::Config.create_config_file_from_template
         if created_config_file_path
-          puts "Default config file created at #{created_config_file_path}."
+          say "Default config file created at #{created_config_file_path}."
         else
-          puts "Config file already exists."
+          say "Config file already exists."
         end
       end
     end
---
+++ spec/woli/cli/runner_init_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+require 'woli/cli/runner'
+
+describe Woli::Cli::Runner do
+  before do
+    module Woli::Config ; end
+    @runner = Woli::Cli::Runner.new
+  end
+  describe "init task" do
+    it "creates configuration for the user and informs them" do
+      Woli::Config.expects(:create_config_file_from_template).returns('/home/woli/.woli/config.yml')
+      @runner.expects(:say).with { |text| text =~ %r{created at /home/woli/\.woli/config\.yml} }
+      @runner.init
+    end
+
+    it "informs the user if the config file is already present" do
+      Woli::Config.expects(:create_config_file_from_template).returns(nil)
+      @runner.expects(:say).with { |text| text =~ %r{already exists} }
+      @runner.init
+    end
+  end
+end
