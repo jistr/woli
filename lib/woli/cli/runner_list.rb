c77fd9837e52ad0e2851ae869c111e6fb8d60896
Jiří Stránský <jistr@jistr.com>
2012-07-18 12:15:35 +0200

    use #say instead of puts in CLI tasks

--- lib/woli/cli/runner_list.rb
+++ lib/woli/cli/runner_list.rb
@@ -4,7 +4,7 @@ module Woli
       desc 'list', 'List dates for which there are entries in the diary.'
       def list
         Woli.diary.all_entries_dates.each do |date|
-          puts date.strftime('%d-%m-%Y')
+          say date.strftime('%d-%m-%Y')
         end
       rescue ConfigError => e
         raise Thor::Error, e.message
--- lib/woli/cli/runner_status.rb
+++ lib/woli/cli/runner_status.rb
@@ -6,17 +6,17 @@ module Woli
         entries_dates = Woli.diary.all_entries_dates
         date_format = '%d-%m-%Y'
 
-        puts "Woli Diary Status"
-        puts "================="
+        say "Woli Diary Status"
+        say "================="
 
         if entries_dates.count > 0
           coverage = entries_dates.count / Float(entries_dates.last - entries_dates.first + 1)
-          puts "First entry: #{entries_dates.first.strftime(date_format)}"
-          puts "Last entry: #{entries_dates.last.strftime(date_format)}"
-          puts "Coverage: %2d %" % (coverage * 100).round
-          puts "Missing entries since the last one: #{Woli.diary.missing_entries_count}"
+          say "First entry: #{entries_dates.first.strftime(date_format)}"
+          say "Last entry: #{entries_dates.last.strftime(date_format)}"
+          say "Coverage: %2d %" % (coverage * 100).round
+          say "Missing entries since the last one: #{Woli.diary.missing_entries_count}"
         else
-          puts "Diary is empty. Run 'woli help edit' to learn how to create entries."
+          say "Diary is empty. Run 'woli help edit' to learn how to create entries."
         end
       rescue ConfigError => e
         raise Thor::Error, e.message
